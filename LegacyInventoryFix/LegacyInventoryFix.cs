7b3eb4ddac71601fa55a9d73228fc15c985d0a03
rassi0429 <taitokokoa+rassi@gmail.com>
2024-08-08 18:04:37 +0900

    v0.0.1

---
+++ LegacyInventoryFix/LegacyInventoryFix.cs
@@ -0,0 +1,67 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using ResoniteModLoader;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LegacyInventoryFix
+{
+    public class LegacyInventoryFix : ResoniteMod
+    {
+        public override string Name => "LegacyInventoryFix";
+        public override string Author => "kka429";
+        public override string Version => "0.0.1";
+        public override string Link => "https://github.com/rassi0429/";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("dev.kokoa.LegacyInventoryFix");
+            harmony.PatchAll();
+        }
+
+
+        [HarmonyPatch(typeof(UserspaceRadiantDash), "ToggleLegacyInventory")]
+        class LegacyInventoryFixPatch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                // show all codes
+                for(int i = 0; i < codes.Count; i++)
+                {
+                    // when   ldc.r4 700 -> ldc.r4 1000
+                    if (codes[i].opcode == System.Reflection.Emit.OpCodes.Ldc_R4 && (float)codes[i].operand == 700f
+                         && codes[i + 1].opcode == System.Reflection.Emit.OpCodes.Ldc_R4 && (float)codes[i + 1].operand == 700f)
+                    {
+                        codes[i].operand = 1140f;
+                        codes[i + 1].operand = 700f;
+                        break;
+                    }
+
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+
+        [HarmonyPatch(typeof(BrowserDialog), "OnAttach")]
+        class InventoryBrowserFixPatch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                // show all codes
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == System.Reflection.Emit.OpCodes.Ldc_R4 && (float)codes[i].operand == 1.2f)
+                    {
+                        codes[i].operand = 1.66667f;
+                        break;
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
